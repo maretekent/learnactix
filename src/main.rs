7f6e6bc062603eaa4e4ed32166c70779af6b16ca
kent marete <maretekent@gmail.com>
2022-09-09 08:56:33 +0300

    Application state is shared with all routes and resources within the same scope.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use actix_web::{ web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
 
+struct AppState {
+	app_name: String,
+}
 
 async fn index () -> impl Responder {
 	log::info!("executing the index method...");
 	HttpResponse::Ok().body("Hello world")
+}
+
+#[get("/state")]
+async fn testState (data: web::Data<AppState>) -> String {
+	let app_name = &data.app_name;
+	format!("Hello {:?}", app_name)
 }
 
 
@@ -15,10 +24,16 @@ async fn main() -> std::io::Result<()> {
 	HttpServer::new( move || {
 		App::new()
 			.wrap(Logger::default())
-			.service(
-				web::scope("/app")
-					.route("/index.html", web::get().to(index))
+			.app_data(
+				web::Data::new(AppState {
+					app_name: String::from("Actix Web"),
+				})
 			)
+			// .service(
+			// 	web::scope("/app")
+			// 		.route("/index.html", web::get().to(index))
+			// )
+			.service(testState)
 	})
 		.bind("0.0.0.0:5001")?
 		.run()
